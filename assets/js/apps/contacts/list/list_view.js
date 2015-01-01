92f25597695ec1c74c146fb72f46d3a27c79e203
Joaquín <jjtarrago@gmail.com>
2015-01-01 15:41:24 +0100

    commit

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -5,7 +5,8 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
 
         events: {
             "click": "highlightName",
-            "click button.js-delete": "deleteClicked"
+            "click button.js-delete": "deleteClicked",
+            "click td a.js-show": "showClicked"
         },
 
         highlightName: function(e) {
@@ -16,6 +17,20 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
         deleteClicked: function(e) {
             e.stopPropagation();
             this.trigger('contact:delete', this.model);
+        },
+
+        showClicked: function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.trigger('contact:show', this.model);
+        },
+
+        remove: function() {
+            var self = this;
+
+            this.$el.fadeOut(function() {
+                Marionette.ItemView.prototype.remove.call(self);
+            });
         }
     });
 
